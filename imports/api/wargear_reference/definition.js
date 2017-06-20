d1633882602ef3bfba8d98d492fbda126401e2ed
Genroa <genroa@hotmail.fr>
2017-06-20 19:04:45 +0200

    Work on Wargear

--- imports/api/wargear_reference/definition.js
+++ imports/api/wargear_reference/definition.js
@@ -1,18 +1,49 @@
 
 
-import { Class } from "meteor/jagi:astronomy";
+import { Class, Union } from "meteor/jagi:astronomy";
 
 
 
-WargearSlotOptions = Object.freeze({
-	
-	GRENADE1:Symbol(),
-	GRENADE2:Symbol(),
-	TENTACLE:Symbol(),
-	WEAPON1:Symbol(),
-	WEAPON2:Symbol(),
-	JUMPPACK:Symbol()
+StringOrNumber = Union.create({
+  name: 'StringOrNumber',
+  types: [String, Number]
 });
+
+ObjectOrNumber = Union.create({
+  name: 'ObjectOrNumber',
+  types: [Number, Object]
+});
+
+
+WargearType = Object.freeze({
+	EQUIPMENT:"EQUIPMENT",
+	WEAPON:"WEAPON"
+});
+
+
+
+
+WeaponProfile = Class.create({
+	name: 'WeaponProfile',
+	fields: {
+		profileName: { // for when a wargear has multiple weapon profiles (combi weapons)
+			type: String,
+			default: function(){ return "";}
+		},
+		type: String,
+		range: Number,
+		S: StringOrNumber, // because S can be "User*2" for instance
+		AP: Number,
+		D: StringOrNumber, // because a number of D can be determined by a dice, "D6",
+		ability: { // weapon profile ability
+			type: String,
+			default: function() {return "";}
+		}
+	}
+});
+
+
+
 
 
 
@@ -20,10 +51,26 @@ WargearReference = Class.create({
 	name: 'WargearReference',
 	collection: new Mongo.Collection('wargear_reference'),
 	fields: {
-		name: String
+		name: String,
+		type: String, // WargearType
+		weaponProfiles: {
+			type: [WeaponProfile],
+			optional: true
+		},
+		ability: { // weapon information 
+			type: String,
+			default: function() {return "";}
+		},
+		profileModifier: {
+			type: StatsModifierContainer,
+			default: function(){return new StatsModifierContainer({});}
+		},
+		cost: ObjectOrNumber
 	},
 	helpers: {
-		
+		computeCost(army) {
+			return 0; // TODO
+		}
 	}
 });
 
